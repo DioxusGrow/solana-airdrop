fd664f0736b58d236d4bf0e496eb3c9f123bb712
DioxusGrow <dioxusgrow@proton.me>
2024-10-01 17:12:10 +0300

    first commit

---
+++ src/constant.rs
@@ -0,0 +1,5 @@
+// const CONNECTION = "";
+// const PUBKEY = "";
+// const CLUSTERAPIURL = "";
+// const KEYPAIR = "";
+// const LAMPORTS_PER_SOL = "";
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+mod constant;
+
+pub const STYLE: &str = asset!("./assets/tailwind.css");
+
+#[derive(PartialEq, Debug, Clone, Copy)]
+enum Validator {
+    Local,
+    Testnet,
+    Devnet,
+    Mainnet,
+}
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        head::Link { rel: "stylesheet", href: STYLE }
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut validator = use_signal(|| Validator::Local);
+    let mut validator_select = use_signal(|| "local".to_string());
+    // let mut validator_memo = use_memo(move || validator_select);
+    let mut url = use_signal(|| "http://127.0.0.1:8899".to_string());
+    rsx! {
+        label { r#for: "validator", "Choose a validator:" }
+        select {
+            name: "validator",
+            id: "validator",
+            value: "{validator_select}",
+            oninput: move |ev| {
+                validator_select.set(ev.value());
+            },
+            option { value: "local", "local" }
+            option { value: "devnet", "devnet" }
+            option { value: "testnet", "testnet" }
+            option { value: "mainnet-beta", "mainnet-beta" }
+        }
+
+        div {
+            {
+                match validator_select.read().as_str()   {
+                    "local" => {
+                        // validator.set(Validator::Local);
+                        url.set("http://127.0.0.1:8899".to_string());
+                    },
+                    "devnet" => {
+                        // validator.set(Validator::Devnet);
+                        url.set("https://api.devnet.solana.com".to_string());
+                    }
+                    "testnet" => {
+                        // validator.set(Validator::Testnet);
+                        url.set("https://api.testnet.solana.com".to_string());
+                    }
+                    "mainnet-beta" => {
+                        // validator.set(Validator::Mainnet);
+                        url.set("https://api.mainnet-beta.solana.com".to_string());
+                    }
+                    _ => {}
+                }
+            },
+
+            "{url()}"
+        }
+    }
+}
+
+// fn SelectValidator(validator_select: Signal<String>) -> Element {
+//     rsx! {
+//         label { r#for: "validator", "Choose a validator:" }
+//         select {
+//             name: "validator",
+//             id: "validator",
+//             value: "{validator_select}",
+//             oninput: move |ev| {
+//                 validator_select. = ev.value();
+//             },
+//             option { value: "local", "local" }
+//             option { value: "devnet", "devnet" }
+//             option { value: "testnet", "testnet" }
+//             option { value: "mainnet-beta", "mainnet-beta" }
+//         }
+//     }
+// }
+
+// mainnet-beta https://api.mainnet-beta.solana.com
+// devnet https://api.devnet.solana.com
+// testnet https://api.testnet.solana.com
+// local "http://127.0.0.1:8899"
+// div {
+//     {
+//         match (validator_memo.read())().as_str()  {
+//             "local" => {
+//                 // validator.set(Validator::Local);
+//                 url.set("http://127.0.0.1:8899".to_string());
+//             },
+//             "devnet" => {
+//                 // validator.set(Validator::Devnet);
+//                 url.set("https://api.devnet.solana.com".to_string());
+//             }
+//             "testnet" => {
+//                 // validator.set(Validator::Testnet);
+//                 url.set("https://api.testnet.solana.com".to_string());
+//             }
+//             "mainnet-beta" => {
+//                 // validator.set(Validator::Mainnet);
+//                 url.set("https://api.mainnet-beta.solana.com".to_string());
+//             }
+//             _ => {}
+//         }
+//     },
+
+//     "{url()}"
+// }
